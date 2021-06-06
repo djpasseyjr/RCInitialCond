b926dff8308af98b4bbcd1d635542e3af0ec325b
Taylor Pool <tpool2@login02.rc.byu.edu>
2021-06-05 23:15:06 -0600

    Added support for creating timestamped result directories

--- Slurm/simple_submit_script.sh
+++ Slurm/simple_submit_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+RCDIR="$(dirname $( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd ) )"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 SYSTEMS=("lorenz" "rossler" "thomas" "softrobot")
@@ -7,12 +7,17 @@ MAP_INITIALS=("random" "activ_f" "relax")
 PREDICTION_TYPES=("continue" "random")
 METHODS=("standard" "augmented")
 
+SYSTEMS=("softrobot")
+
 for system in ${SYSTEMS[@]}; do
         for map_initial in ${MAP_INITIALS[@]}; do
                 for prediction_type in ${PREDICTION_TYPES[@]}; do
                         for method in ${METHODS[@]}; do
-                                FLAGS="--ntasks=1 --cpus-per-task=1 --time=3:00:00 --chdir=data"
-                                sbatch $SCRIPT_DIR/opt_then_test_batch.sh $system $map_initial $prediction_type $method
+                                TIMESTAMP=$(date +"%m-%d-%y:%H:%M:%S")
+                                LOGDIR=$RCDIR"/Data/"$system"_"$map_initial"_"$prediction_type"_"$method"_"$TIMESTAMP
+                                mkdir $LOGDIR
+                                FLAGS="-o $LOGDIR/slurm-%a.out --time=04:00:00 --ntasks=1 --nodes=1 --mem-per-cpu=1024M -J "$system"_"$map_initial"_"$prediction_type"_"$method
+                                sbatch $FLAGS $SCRIPT_DIR/opt_then_test_batch.sh $system $map_initial $prediction_type $method $LOGDIR
                         done
                 done
         done
