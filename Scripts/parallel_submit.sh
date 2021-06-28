4bc1e4227cb9538f04045b8da42d2b864a893509
lostinnowhere314 <quinlan.leishman@gmail.com>
2021-06-28 17:12:35 -0600

    Example slurm script for opt_then_test.py in parallel

---
+++ Scripts/parallel_submit.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#SBATCH --mail-user=quinlan.leishman@gmail.com   # email address
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+
+SYSTEM=$1
+MAP_INITIAL=$2
+PREDICTION_TYPE=$3
+METHOD=$4
+LOGDIR=$5
+
+# Set the max number of threads to use for programs using OpenMP. Should be <= ppn.
+export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
+
+profile=slurm_${SLURM_JOB_ID}_$(hostname)
+
+#This method for using ipyparallel with slurm was taken from https://k-d-w.org/blog/2015/05/using-ipython-for-parallel-computing-on-an-mpi-cluster-using-slurm/
+echo "Creating profile ${profile}"
+ipython profile create ${profile}
+
+rundir=$(pwd)
+cd ~
+
+echo "Launching controller"
+.local/bin/ipcontroller --ip="*" --profile=${profile} --log-to-file &
+sleep 10
+
+echo "Launching engines"
+srun .local/bin/ipengine --profile=${profile} --location=$(hostname) --log-to-file &
+sleep 30
+
+cd $rundir
+
+echo "Launching job"
+python opt_then_test.py $SYSTEM $MAP_INITIAL $PREDICTION_TYPE $METHOD $LOGDIR --parallel=${profile}
