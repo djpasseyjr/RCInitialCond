1caf33aad4b44f30c368ef54e9e7304bda140b64
lostinnowhere314 <quinlan.leishman@gmail.com>
2022-04-11 12:41:09 -0600

    Finish the script

--- Scripts/NewOptimize/new_optimize.py
+++ Scripts/NewOptimize/new_optimize.py
@@ -1,23 +1,41 @@
 import sys
 import numpy as np
 from rescomp import optimizer as rcopt
+import dill as pickle
 
-def main(system, aug_type, pred_type, mean_degree, n):
+def main(system, aug_type, pred_type, init_cond, mean_degree, n, parallel_profile=None):
     """
     aug_type - augmented or standard
     pred_type - continue or random
     """
     mean_degree = float(mean_degree)
     n = int(n)
-    system = rcopt.get_system(system)
+    
     optimizer = rcopt.ResCompOptimizer(
-        system, 'TODO', pred_type, aug_type,
+        system, init_cond, pred_type, aug_type,
         rm_params = ['mean_degree'],
+        parallel_profile=parallel_profile,
         # Rescomp parameters
         mean_degree = mean_degree,
-        res_sz = n
+        res_sz = n,
+        ##
     )
     
+    # Run the optimization
+    optimizer.run_optimization(
+        opt_ntrials=100, 
+        vpt_reps=256
+    )
+    
+    # Best params
+    best_params = optimizer.get_best_result()
+    
+    # Save
+    with open(result_file, 'wb') as file:
+        pickle.dump((
+            (system, aug_type, pred_type, init_cond, mean_degree, n),
+            best_params
+        ), file)
 
 if __name__=="__main__":
     main(*sys.argv[1:])
