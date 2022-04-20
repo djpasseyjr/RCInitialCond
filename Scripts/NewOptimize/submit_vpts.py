49db630fec29750b53e374eec784529247a7cf7e
Taylor Pool <taylorpool.27@gmail.com>
2022-04-20 11:53:47 -0600

    Added vpt file

---
+++ Scripts/NewOptimize/submit_vpts.py
@@ -0,0 +1,44 @@
+import sys
+import numpy as np
+import rescomp as rc
+import pandas as pd
+import subprocess
+
+if __name__ == '__main__':
+    data_dir = "vpt_results"
+    ntasks = 16
+    timelimit_hr = 72
+
+    subprocess.run(['mkdir', data_dir])
+    subprocess.run(['mkdir', f"{data_dir}/logfiles"])
+
+    # Load dataframe
+    df = pd.read_pickle(sys.argv[1])
+
+    for x in df.values:
+        args = (
+            'vpt.py', 
+            x[0], 
+            x[1], 
+            x[2], 
+            x[3], 
+            str(x[4]),
+            str(x[5]),
+            str(x[6]),
+            str(x[7]),
+            str(x[8]),
+            str(x[9]),
+            str(x[10]),
+            str(x[11])
+        )
+
+        flags = (
+            '-o', f'{data_dir}/logfiles/slurm-%a.out',
+            '-t', '{}:00:00'.format(timelimit_hr),
+            '--ntasks={}'.format(ntasks),
+            '--nodes=1',
+            '--mem-per-cpu=1G',
+            '-J', '-'.join(args[1:-1])
+        )
+
+        subprocess.run(['sbatch', *flags, 'parallel_submit.sh', *args])
---
+++ Scripts/NewOptimize/vpt.py
@@ -0,0 +1,49 @@
+import sys
+import rescomp as rc
+from rescomp import optimizer as rcopt
+import pickle
+
+data_dir = 'vpt_results'
+ntrials = 8092
+
+if __name__=='__main__':
+    # system     is_aug pred_type   ic_map  mean_degree     n      gamma   ridge_alpha     sigma  spect_rad  overlap window
+    # Get the experiment
+    system = sys.argv[1]
+    aug_type = sys.argv[2]
+    pred_type = sys.argv[3]
+    icmap = sys.argv[4]
+    mean_deg = float(sys.argv[5])
+    n = int(sys.argv[6])
+
+    params = dict()
+    params['mean_degree'] = mean_deg
+    params['res_sz'] = n
+    # Get the other parameters
+    params['gamma'] = float(sys.argv[7])
+    params['ridge_alpha'] = float(sys.argv[8]) 
+    params['sigma'] = float(sys.argv[9]) 
+    params['spect_rad'] = float(sys.argv[10]) 
+    if aug_type == 'augmented': 
+        params['overlap'] = float(sys.argv[11]) 
+        params['window'] = float(sys.argv[12]) 
+
+    parallel_profile = sys.argv[-1]
+
+    # Set up and run the jobs
+    optimizer = rcopt.ResCompOptimizer(
+        system, icmap, pred_type, aug_type,
+        parallel_profile=parallel_profile,
+        results_directory=data_dir,
+    )
+
+    results = optimizer.run_tests(ntrials, lyap_reps=1, parameters=params)
+
+    # Save result file
+    result_filename = data_dir + '/{}-{}-{}-{}-d{}-n{}-vpts.pkl'.format(system, aug_type, pred_type, init_cond, mean_degree, n)
+    with open(result_filename, 'wb') as file:
+        pickle.dump((
+            (system, aug_type, pred_type, icmap, mean_deg, n),
+            results
+        ), file)
+    
