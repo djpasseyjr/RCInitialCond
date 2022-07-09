1d1cf5f28bda54a77ad89dc3a6be99ef38e30761
lostinnowhere314 <quinlan.leishman@gmail.com>
2022-07-09 13:28:30 -0600

    add submit command

--- Scripts/NewOptimize/submit_compare_training.py
+++ Scripts/NewOptimize/submit_compare_training.py
@@ -59,6 +59,5 @@ if __name__=="__main__":
         )
         
         #Submit the job
-        print('sbatch', *flags, 'parallel_submit.sh', *args)
-        #subprocess.run(['sbatch', *flags, 'parallel_submit.sh', *args])
+        subprocess.run(['sbatch', *flags, 'parallel_submit.sh', *args])
         
