c625f60989dd4c1a6dd0efa848d2ece2dbc39b06
lostinnowhere314 <quinlan.leishman@gmail.com>
2022-07-19 13:20:02 -0600

    Update zip-data.sh

--- Scripts/NewOptimize/zip_data.sh
+++ Scripts/NewOptimize/zip_data.sh
@@ -1,6 +1,6 @@
 
 echo 'Zipping data...'
 
-zip -ru `date +"results-%y-%m-%d.zip"` results/*.pkl vpt_results/*.pkl attractor_results/*.pkl
+zip -r `date +"results-%y-%m-%d.zip"` results/*.pkl vpt_results/*.pkl attractor_results/*.pkl traintimes/vpts/*.pkl traintimes/optimization/*.pkl
 
 echo 'Done'
